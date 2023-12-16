2b09b138c28734c793d21821159dd6bfa2b1f9c3
Craig Labenz <craig.labenz@gmail.com>
2023-12-16 10:39:30 -0800

    adds client_data

--- example/client_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/client_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,11 +8,9 @@ import Foundation
 import desktop_webview_auth
 import firebase_auth
 import firebase_core
-import sign_in_with_apple
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DesktopWebviewAuthPlugin.register(with: registry.registrar(forPlugin: "DesktopWebviewAuthPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
 }
---
+++ packages/client_data/lib/client_data.dart
@@ -0,0 +1,4 @@
+/// Handles low-level networking / data loading.
+library client_data;
+
+export 'src/app_config/app_config.dart';
